c30d03275a72124338cddc3ad4bda66fbeeba1fa
astrogeek20 <astrogeek20@gmail.com>
2017-01-21 11:57:13 -0500

    Change KEY

--- app/src/main/java/com/example/astrogeek20/cravings21/MainActivity.java
+++ app/src/main/java/com/example/astrogeek20/cravings21/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.astrogeek20.cravings21;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Show(View view)
+    {
+        String button_txt;
+        button_txt = ((Button) view).getText().toString();
+        if (button_txt.equals("Click to access Map"))
+        {
+            Intent intent =new Intent(this, MapsActivity.class);
+                    startActivity(intent);
+        }
+    }
+
 }
